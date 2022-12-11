1db8af21474775799b370e283cbc5e92368cc83e
jenny7742 <akpanjennifer4@gmail.com>
2022-12-11 05:53:46 -0800

    puts 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 
 /**
  * main - Entry point
- * Return: always 0 (success)
+ *Return: always 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
