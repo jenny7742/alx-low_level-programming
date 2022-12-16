c87581e1f31cbb628ad6c845656bc7d1246a93a5
jenny7742 <akpanjennifer4@gmail.com>
2022-12-16 12:18:30 -0800

    hot fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,27 @@
 #include "main.h"
+#include <unistd.h>
 
+
+
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ *
+ *
+ * Return: On success 1.
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+
+int _putchar(char c)
+{
+
+		return (write(1, &c, 1));
+
+}
 /**
  * print_most_numbers - prints from 0 to 9
  *
