1861e1945a984ec202ecf24a357e5bbbb30ccc66
jenny7742 <akpanjennifer4@gmail.com>
2022-12-16 12:36:52 -0800

    not even numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,27 +1,6 @@
+#include <stdio.h>
 #include "main.h"
-#include <unistd.h>
 
-
-
-/**
- * _putchar - writes the character c to stdout
- *
- * @c: The character to print
- *
- *
- *
- * Return: On success 1.
- *
- * On error, -1 is returned, and errno is set appropriately.
- *
- */
-
-int _putchar(char c)
-{
-
-		return (write(1, &c, 1));
-
-}
 /**
  * print_most_numbers - prints from 0 to 9
  *
@@ -37,8 +16,8 @@ void print_most_numbers(void)
 		{
 			continue;
 		}
-	_putchar(letter);
+	putchar(letter);
 	}
-	_putchar(10);
+	putchar(10);
 
 }
