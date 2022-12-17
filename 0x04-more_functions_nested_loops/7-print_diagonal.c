2839f01dca9fda9df3cfa4255e92aff7a72353d0
jenny7742 <akpanjennifer4@gmail.com>
2022-12-16 16:36:37 -0800

    diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include " main.h"
+
+/**
+ * print_diagonal - prints lines and dashes
+ * @n: parameter
+ * return: always 0
+ */
+void print_diagonal(int n)
+{
+	int x;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i <= x; i++)
+				putchar(' ');
+			putchar(95);
+			putchar('\n');
+
+		}
+		putchar ('\n');
+	}
+}
