46cbb2d73b5736eb88977a606c6c2fd5dcaa9133
jenny7742 <akpanjennifer4@gmail.com>
2022-12-20 15:22:39 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every second character
+ * @str: parameter
+ */
+void puts2(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	for (i = 0; i < len; i += 2)
+	{
+		putchar(p[i]);
+	}
+}
