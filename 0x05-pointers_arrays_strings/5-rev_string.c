39724de7ee36370a57e2e193db2c5e6b2b0b1693
jenny7742 <akpanjennifer4@gmail.com>
2022-12-19 17:35:35 -0800

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - mirror view
+ * @s: parameter
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length] = '0')
+		length1++;
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length -= 1;
+	}
+}
