aaf33e11f4785b7688672b92e0bcfef0ef07ec3b
jenny7742 <akpanjennifer4@gmail.com>
2022-12-22 06:38:56 -0800

    stncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,6 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int x = 0;
-
-	while (dest[x])
-		x++;
-	for (n = 0; src[n] != '\0'; n++)
-	{
-		dest[x] = src[n];
-		x += 1;
-	}
-	dest[x] = '\0';
+	strncat(*dest, *src, n);
 	return (dest);
 }
