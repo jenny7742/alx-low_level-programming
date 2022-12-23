58503ffdbe793904c7d9d4d69f01e780238093c1
jenny7742 <akpanjennifer4@gmail.com>
2022-12-23 07:57:13 -0800

    6-cap_string.c
    git add .
    git add .
    
    gcc -Wall 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all wordsd
+ * @str: parameter
+ * Return: returns str
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= '2'))
+			index++;
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str -= 32;
+		index++;
+
+	}
+	return (str);
+}
