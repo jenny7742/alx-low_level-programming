5d5f0840755f7362b2a09b7876614cf28341d13d
jenny7742 <akpanjennifer4@gmail.com>
2022-12-25 02:51:30 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - prints three digits combinations
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int x;
+	int a;
+
+	i = 0;
+	while (i < 10)
+	{
+		x = 0;
+		while (x < 10)
+		{
+			a = 0;
+			while (a < 10)
+			{
+				if (i != x && i != a && a != x &&
+				    i < x && i < a && x < a)
+				{
+					putchar(48 + i);
+					putchar(48 + x);
+					putchar(48 +  a);
+
+					if (i + a != 17 && x + i != 17)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				a++;
+			}
+			x++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
