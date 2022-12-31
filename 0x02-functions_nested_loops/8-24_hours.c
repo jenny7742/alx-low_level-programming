9d0729039a0776eb5e74869c9b8d45f90d2a412f
jenny7742 <akpanjennifer4@gmail.com>
2022-12-31 09:11:03 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,16 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for(hour = 0; hour <= 23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for(minute = 0; minute < 59; minute++)
+		for (minute = 0; minute < 59; minute++)
 		{
 			putchar(hour / 10 + '0');
 			putchar(hour % 10 + '0');
 			putchar(':');
 			putchar(minute / 10 + '0');
 			putchar(minute % 10 + '0');
+			putchar('\n');
 		}
 	}
 }
