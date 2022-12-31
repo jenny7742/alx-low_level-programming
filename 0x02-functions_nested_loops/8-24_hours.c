e630ba1226019ef87c82278d47504d44b3913380
jenny7742 <akpanjennifer4@gmail.com>
2022-12-31 09:04:27 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prin ts every minute
+ * Return: no return
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for(hour = 0; hour <= 23; hour++)
+	{
+		for(minute = 0; minute < 59; minute++)
+		{
+			putchar(hour / 10 + '0');
+			putchar(hour % 10 + '0');
+			putchar(':');
+			putchar(minute / 10 + '0');
+			putchar(minute % 10 + '0');
+		}
+	}
+}
