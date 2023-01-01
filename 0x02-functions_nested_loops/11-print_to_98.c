95d1d7067b092e5a0a8f01999d435b33e009d70f
jenny7742 <akpanjennifer4@gmail.com>
2022-12-31 16:31:17 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,51 +3,21 @@
 
 /**
  * print_to_98 - prints to 98
- * @x: parameter
+ * @n: parameter
  * Return:  no retur
  */
 void print_to_98(int n)
 {
-	int x = 98;
-
-	if (n < x)
+	if (n >= 98)
 	{
-		putchar(n + '0');
-		n++;
-
-		if (n != 98)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		else
-		{
-			putchar('\n');
-		}
-	}
-	else if (n > x)
-	{
-		putchar(n + '0');
-		n--;
-
-		if (n != 98)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		else
-		{
-			putchar('\n');
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d, ", n);
 	}
 	else
 	{
-		putchar(n + '0');
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d, ", n++);
 	}
-}
-int main()
-{
-	print_to_98(50);
-
-	return (0);
 }
