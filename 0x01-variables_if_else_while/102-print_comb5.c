4571d5177b7fc8962c57b691cb54f5668571cf9b
jenny7742 <akpanjennifer4@gmail.com>
2023-01-03 01:47:27 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,16 +15,19 @@ int main(void)
 		b = 0;
 		while (b <= 99)
 		{
-			putchar(a / 10 + '0');
-			putchar(a % 10 + '0');
-			putchar(' ');
-			putchar(b / 10 + '0');
-			putchar(b % 10 + '0');
-
-			if (a != 98 || b != 99)
+			if (b > a)
 			{
-				putchar(',');
+				putchar(a / 10 + '0');
+				putchar(a % 10 + '0');
 				putchar(' ');
+				putchar(b / 10 + '0');
+				putchar(b % 10 + '0');
+
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			b += 1;
 		}
