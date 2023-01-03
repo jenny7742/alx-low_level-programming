f5f3d6670eea4d6d7ac5c6f5d109f2f17b42536d
jenny7742 <akpanjennifer4@gmail.com>
2023-01-03 14:21:45 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,21 +8,5 @@
  */
 void print_number(int n)
 {
-	unsigned int m;
-
-	if (m < 0)
-	{
-		putchar('-');
-		m = -n;
-	}
-	else
-	{
-		m = n;
-	}
-
-	if ((m / 10) != 0)
-	{
-		print_number(m / 10);
-	}
-	putchar((m % 10) + '0');
+	un
 }
