43f0abc0ce04182bb94d8617a670d6b829e44443
jenny7742 <akpanjennifer4@gmail.com>
2023-01-03 09:27:30 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,31 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
-
-/**
- * positive_or_negative - sign checker
- * @i: parameter
- * Return: no return
- */
-
-void positive_or_negative(int i)
-{
-	int n;
-
-
-
-	srand(time(0));
-
-	n = i;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-}
 
 /**
  * main - test for positive or negative integers
