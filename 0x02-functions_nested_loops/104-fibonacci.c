083534e9b22bcbe621c40860d159c9f375df0031
jenny7742 <akpanjennifer4@gmail.com>
2023-01-03 02:15:03 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,43 +8,43 @@
  */
 int main(void)
 {
-	unsigned long n1 = 0, n2 = 1, n-sum;
-	unsigned long n1-hf1, n1-hf2, n2-hf1, n2-hf2;
+	unsigned long n1 = 0, n2 = 1, n_sum;
+	unsigned long n1_hf1, n1_hf2, n2_hf1, n2_hf2;
 	unsigned long hf1, hf2;
 	int wax;
 
-	for (wax =0; wax < 92; wax++)
+	for (wax = 0; wax < 92; wax++)
 	{
-		n-sum = n1 + n2;
-		printf("%lu, ", n-sum);
+		n_sum = n1 + n2;
+		printf("%lu, ", n_sum);
 
 		n1 = n2;
-		n2 = n-sum;
+		n2 = n_sum;
 	}
 
-	n1-hf1 = n1 / 10000000;
-	n2-hf1 = n2 / 10000000;
-	n1-hf2 = n1 % 10000000;
-	n2-hf2 = n2 % 10000000;
+	n1_hf1 = n1 / 10000000;
+	n2_hf1 = n2 / 10000000;
+	n1_hf2 = n1 % 10000000;
+	n2_hf2 = n2 % 10000000;
 
 	for (wax = 93; wax < 99; wax++)
 	{
-		hf1 = n1-hf1 + n2-hf1;
-		hf2 = n1-hf2 + n2-hf2;
-		if (n1-hf2 + n2-hf2 > 9999999999)
+		hf1 = n1_hf1 + n2_hf1;
+		hf2 = n1_hf2 + n2_hf2;
+		if (n1_hf2 + n2_hf2 > 9999999999)
 		{
 			hf1 += 1;
 			hf2 %= 10000000000;
 		}
 
 		printf("%lu%lu", hf1, hf2);
-		if (count != 98)
+		if (wax != 98)
 			printf(", ");
 
-		n1-hf1 = n2-hf1;
-		n1-hf2 = n2-hf2;
-		n2-hf1 = hf1;
-		n2-hf2 = hf2;
+		n1_hf1 = n2_hf1;
+		n1_hf2 = n2_hf2;
+		n2_hf1 = hf1;
+		n2_hf2 = hf2;
 	}
 	printf("\n");
 
