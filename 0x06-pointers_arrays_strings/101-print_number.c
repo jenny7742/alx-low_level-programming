494a5afd2f31e2bab225645bd08c81a5475a97b8
jenny7742 <akpanjennifer4@gmail.com>
2023-01-03 14:32:21 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,5 +8,29 @@
  */
 void print_number(int n)
 {
-	un
+	int res, temp, expo;
+
+
+	expo = 1;
+
+	if (n >= 0)
+		res = n * -1;
+	else
+	{
+		res = n;
+		putchar('-');
+	}
+
+	temp = res;
+	while (temp <= -10)
+	{
+		expo *= 10;
+		temp /= 10;
+	}
+
+	while (expo >= 1)
+	{
+		putchar(((res / expo) % 10) * -1 + '0');
+		expo /= 10;
+	}
 }
