a810afc2e29c51813a530e25387661ead843808a
jenny7742 <akpanjennifer4@gmail.com>
2023-01-03 16:14:28 -0800

    3-strbrk

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - searhces for a stinrg
+ * @s: parameter
+ * @accept: parmetr
+ * Return: return 0
+ */
+char *-strphbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (a < 10)
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+
+}
